4f858c466a75836f0a4b2e5e5f921fcfb98cf873
Aroaxer <137940818+Aroaxer@users.noreply.github.com>
2024-04-04 19:33:00 +0000

    Renamed "upgraded" to "tier one" and added a few new items

--- premades.py
+++ premades.py
@@ -9,39 +9,42 @@ acs = {
     # Physical Melee
     "Basic Attack" : Action("Basic Attack", "1x Damage"),
     "Slam" : Action("Slam", "1.5x Damage", maxCharges=1),
-    "Slash" : Action("Slash", "1x Damage", targeting="Directional", maxCharges=1, width=3),
-    "Impale" : Action("Impale", "1x Damage", targeting="Directional", maxCharges=1, length=3),
+    "Slash" : Action("Slash", "1x Damage", "Directional", maxCharges=1, width=3),
+    "Impale" : Action("Impale", "1x Damage", "Directional", maxCharges=1, length=3),
     "Ritual Stab" : Action("Ritual Stab", "Ritual Attack", maxCharges=2, recharge="Encounter"),
-    "Whirl" : Action("Whirl", "1x Damage", targeting="Centered", maxCharges=5, rechargePercent=0.2, width=3),
+    "Whirl" : Action("Whirl", "1x Damage", "Centered", maxCharges=5, rechargePercent=0.2, width=3),
     
     # Physical Ranged
     "Basic Shot" : Action("Basic Shot", "1x Damage", range=3),
-    "Pierce" : Action("Pierce", "1x Damage", targeting="Directional", maxCharges=1, length=5),
+    "Pierce" : Action("Pierce", "1x Damage", "Directional", maxCharges=1, length=5),
     "Shatter" : Action("Shatter", "1.5x Damage", maxCharges=1, range=3),
-    "Shatterwave" : Action("Shatterwave", "1.5x Damage", targeting="Directional", maxCharges=2, rechargePercent=0.5, width=1, length=5),
+    "Shatterwave" : Action("Shatterwave", "1.5x Damage", "Directional", maxCharges=2, rechargePercent=0.5, width=1, length=5),
+    "Throw" : Action("Throw", "1x Damage", range=2),
     "Toss" : Action("Toss", "1x Damage", maxCharges=1, range=2, freeAction=True),
-    "Twin Shot" : Action("Twin Shot", "0.5x Damage", targeting="Multi 2", range=3),
+    "Twin Shot" : Action("Twin Shot", "0.5x Damage", "Multi 2", range=3),
 
     # Magical
     "Firebolt" : Action("Firebolt", "1x Damage", maxCharges=1, range=3),
-    "Fireball" : Action("Fireball", "1x Damage", targeting="Point", maxCharges=1, rechargePercent=0.5, range=3, width=3),
+    "Fireball" : Action("Fireball", "1x Damage", "Point", maxCharges=1, rechargePercent=0.5, range=3, width=3),
     "Enhanced Firebolt" : Action("Enhanced Firebolt", "1x Damage", range=5),
-    "Flamethrower" : Action("Flamethrower", "1x Damage", targeting="Directional", maxCharges=2, rechargePercent=0.5, length=4),
-    "Arcane Rays" : Action("Arcane Rays", "1x Damage", targeting="Multi 2", maxCharges=5, rechargePercent=0.2, range=3),
-    "Magic Missiles" : Action("Magic Missiles", "1x Damage", targeting="Multi 4", maxCharges=1, rechargePercent=0.5, range=4),
+    "Flamethrower" : Action("Flamethrower", "1x Damage", "Directional", maxCharges=2, rechargePercent=0.5, length=4),
+    "Arcane Rays" : Action("Arcane Rays", "1x Damage", "Multi 2", maxCharges=5, rechargePercent=0.2, range=3),
+    "Magic Missiles" : Action("Magic Missiles", "1x Damage", "Multi 4", maxCharges=1, rechargePercent=0.5, range=4),
+    "Call Lightning" : Action("Call Lightning", "1x Damage", "Point", maxCharges=2, rechargePercent=0.25, range=5, width=3),
 
     # Utility
-    "Hasten" : Action("Hasten", "Hasten", targeting="Self", maxCharges=2, recharge="Encounter"),
+    "Hasten" : Action("Hasten", "Hasten", "Self", maxCharges=2, recharge="Encounter"),
     "Shove" : Action("Shove", "Repel 3"),
     "Pull" : Action("Pull", "Pull", maxCharges=1, range=4, freeAction=True),
     "Double Hook" : Action("Double Hook", "Pull", "Multi 2", maxCharges=1, range=5, freeAction=True),
-    "Teleport" : Action("Teleport", "Teleport", targeting="Point No Enemy", range=10),
+    "Teleport" : Action("Teleport", "Teleport", "Point No Enemy", range=10),
     "Charge" : Action("Charge", "Charge", "Point No Enemy", range=5),
 
     # Defensive
     "Block" : Action("Block", "Block", "Self"),
     "Parry" : Action("Parry", "Parry", "Self", maxCharges=3, recharge="Encounter", freeAction=True),
-    "Arcane Shield" : Action("Arcane Shield", "Invuln", targeting="Self", maxCharges=2, recharge="Encounter", rechargePercent=0.5)
+    "Repel" : Action("Repel", "Repel 1", "Centered", maxCharges=1, rechargePercent=0.5, width=5, freeAction=True),
+    "Arcane Shield" : Action("Arcane Shield", "Invuln", "Self", maxCharges=2, recharge="Encounter", rechargePercent=0.5)
 }
 
 enemAcs = {
@@ -61,15 +64,17 @@ traits = {
 
 # Equips
 # This must be defined in this order
-upgradedWeps = {
+tier2Weps = {
     # Physical Melee
     "Wind Blade" : Equip("Wind Blade", 10, 0, [acs["Basic Attack"], acs["Slash"], acs["Whirl"]]),
     "Flamehammer" : Equip("Flamehammer", 10, 0, [acs["Basic Attack"], acs["Slam"], acs["Fireball"]]),
     "Runed Halberd" : Equip("Runed Halberd", 10, 0, [acs["Basic Attack"], acs["Impale"], acs["Slash"], acs["Firebolt"]]),
+    "Defender Flail" : Equip("Defender Flail", 9, 20, [acs["Basic Attack"], acs["Charge"], acs["Parry"]]),
 
     # Physical Ranged
     "Twinshot Bow" : Equip("Twinshot Bow", 8, 0, [acs["Twin Shot"], acs["Pierce"], acs["Shove"]]),
     "Handheld Ballista" : Equip("Handheld Ballista", 8, 0, [acs["Basic Shot"], acs["Shatterwave"]]),
+    "Storm Spear" : Equip("Storm Spear", 7, 15, [acs["Throw"], acs["Impale"], acs["Call Lightning"]]),
 
     # Magical Ranged
     "Inferno Cane" : Equip("Inferno Cane", 8, 0, [acs["Enhanced Firebolt"], acs["Fireball"], acs["Flamethrower"]]),
@@ -77,17 +82,19 @@ upgradedWeps = {
 }
 weps = {
     # Physical Melee
-    "Sword" : Equip("Sword", 5, 0, [acs["Basic Attack"], acs["Slash"]], upgradedForm = upgradedWeps["Wind Blade"]),
-    "Hammer" : Equip("Hammer", 5, 0, [acs["Basic Attack"], acs["Slam"]], upgradedForm = upgradedWeps["Flamehammer"]),
-    "Spear" : Equip("Spear", 5, 0, [acs["Basic Attack"], acs["Impale"]], upgradedForm = upgradedWeps["Runed Halberd"]),
+    "Sword" : Equip("Sword", 5, 0, [acs["Basic Attack"], acs["Slash"]], upgradedForm = tier2Weps["Wind Blade"]),
+    "Hammer" : Equip("Hammer", 5, 0, [acs["Basic Attack"], acs["Slam"]], upgradedForm = tier2Weps["Flamehammer"]),
+    "Spear" : Equip("Spear", 5, 0, [acs["Basic Attack"], acs["Impale"]], upgradedForm = tier2Weps["Runed Halberd"]),
+    "Mace" : Equip("Mace", 4, 10, [acs["Basic Attack"], acs["Charge"]], upgradedForm = tier2Weps["Defender Flail"]),
 
     # Physical Ranged
-    "Bow" : Equip("Bow", 4, 0, [acs["Basic Shot"], acs["Pierce"]], upgradedForm = upgradedWeps["Twinshot Bow"]),
-    "Crossbow" : Equip("Crossbow", 4, 0, [acs["Basic Shot"], acs["Shatter"]], upgradedForm = upgradedWeps["Handheld Ballista"]),
+    "Bow" : Equip("Bow", 4, 0, [acs["Basic Shot"], acs["Pierce"]], upgradedForm = tier2Weps["Twinshot Bow"]),
+    "Crossbow" : Equip("Crossbow", 4, 0, [acs["Basic Shot"], acs["Shatter"]], upgradedForm = tier2Weps["Handheld Ballista"]),
+    "Javelin" : Equip("Javelin", 4, 5, [acs["Throw"], acs["Impale"]], upgradedForm = tier2Weps["Storm Spear"]),
 
     # Magical Ranged
-    "Fire Staff" : Equip("Fire Staff", 4, 0, [acs["Firebolt"], acs["Fireball"]], upgradedForm = upgradedWeps["Inferno Cane"]),
-    "Arcane Staff" : Equip("Arcane Staff", 2, 0, [acs["Arcane Rays"], acs["Magic Missiles"]], upgradedForm = upgradedWeps["Archmage Rod"])
+    "Fire Staff" : Equip("Fire Staff", 4, 0, [acs["Firebolt"], acs["Fireball"]], upgradedForm = tier2Weps["Inferno Cane"]),
+    "Arcane Staff" : Equip("Arcane Staff", 2, 0, [acs["Arcane Rays"], acs["Magic Missiles"]], upgradedForm = tier2Weps["Archmage Rod"])
 }
 
 enemWeps = {
@@ -101,33 +108,35 @@ enemWeps = {
 }
 
 # Other Helds
-upgradedOffs = {
+tier2Offs = {
     # Offensive
     "Ritual Blade" : Equip("Ritual Blade", 8, 0, [acs["Ritual Stab"], acs["Pull"]]),
     "Chain Hooks" : Equip("Chain Hooks", 3, 0, [acs["Double Hook"]]),
     "Shrouded Dagger" : Equip("Shrouded Dagger", 5, 0, [acs["Toss"], acs["Parry"]]),
 
     # Defensive
-    "Crystal Shield" : Equip("Crystal Shield", 2, 20, [acs["Block"], traits["Spikes"]])
+    "Crystal Shield" : Equip("Crystal Shield", 2, 20, [acs["Block"], traits["Spikes"]]),
+    "Castle Shield" : Equip("Castle Shield", 0, 30, [acs["Block"], acs["Repel"]])
 
     # Utility
 
 }
 offs = {
     # Offensive
-    "Ritual Dagger" : Equip("Ritual Dagger", 4, 0, [acs["Ritual Stab"]], upgradedForm = upgradedOffs["Ritual Blade"]),
-    "Rope Hook" : Equip("Rope Hook", 1, 0, [acs["Pull"]], upgradedForm = upgradedOffs["Chain Hooks"]),
-    "Kunai" : Equip("Kunai", 2, 0, [acs["Toss"]], upgradedForm = upgradedOffs["Shrouded Dagger"]),
+    "Ritual Dagger" : Equip("Ritual Dagger", 4, 0, [acs["Ritual Stab"]], upgradedForm = tier2Offs["Ritual Blade"]),
+    "Rope Hook" : Equip("Rope Hook", 1, 0, [acs["Pull"]], upgradedForm = tier2Offs["Chain Hooks"]),
+    "Kunai" : Equip("Kunai", 2, 0, [acs["Toss"]], upgradedForm = tier2Offs["Shrouded Dagger"]),
 
     # Defensive
-    "Shield" : Equip("Shield", 0, 10, [acs["Block"]], upgradedForm = upgradedOffs["Crystal Shield"])
+    "Kite Shield" : Equip("Kite Shield", 0, 10, [acs["Block"]], speedBoost=1, upgradedForm = tier2Offs["Crystal Shield"]),
+    "Tower Shield" : Equip("Tower Shield", 0, 15, [acs["Block"]], upgradedForm = tier2Offs["Castle Shield"])
 
     # Utility
     
 }
 
 # Armor
-upgradedArmors = {
+tier2Armors = {
     # Defensive
     "Battlerager Mail" : Equip("Battlerager Mail", 5, 25, [traits["Spikes"], acs["Charge"]]),
     "Druidic Plate" : Equip("Druidic Plate", 0, 40, [traits["Regenerate"], acs["Block"]]),
@@ -138,12 +147,12 @@ upgradedArmors = {
 }
 armors = {
     # Defensive
-    "Spiked Mail" : Equip("Spiked Mail", 2, 15, [traits["Spikes"]], upgradedForm = upgradedArmors["Battlerager Mail"]),
-    "Overgrown Plate" : Equip("Overgrown Plate", 0, 25, [traits["Regenerate"]], upgradedForm = upgradedArmors["Druidic Plate"]),
-    "Heavy Plate" : Equip("Heavy Plate", 0, 40, [traits["Chain Reduction"]], speedBoost=-1, upgradedForm = upgradedArmors["Titanic Plate"]),
+    "Spiked Mail" : Equip("Spiked Mail", 2, 15, [traits["Spikes"]], upgradedForm = tier2Armors["Battlerager Mail"]),
+    "Overgrown Plate" : Equip("Overgrown Plate", 0, 25, [traits["Regenerate"]], upgradedForm = tier2Armors["Druidic Plate"]),
+    "Heavy Plate" : Equip("Heavy Plate", 0, 40, [traits["Chain Reduction"]], speedBoost=-1, upgradedForm = tier2Armors["Titanic Plate"]),
 
     # Utility
-    "Swift Leather" : Equip("Swift Leather", 0, 0, [], speedBoost=2, actionBoost=1, upgradedForm = upgradedArmors["Leathers of the Wind"])
+    "Swift Leather" : Equip("Swift Leather", 0, 0, [], speedBoost=2, actionBoost=1, upgradedForm = tier2Armors["Leathers of the Wind"])
 }
 
 enemArmrs = {
@@ -152,7 +161,7 @@ enemArmrs = {
 }
 
 # Accesories
-upgradedAccs = {
+tier2Accs = {
     # Defensive
     "Barrier Necklace" : Equip("Barrier Necklace", 0, 10, [acs["Arcane Shield"], traits["Chain Reduction"]]),
 
@@ -163,30 +172,30 @@ upgradedAccs = {
 }
 accs = {
     # Defensive
-    "Brooch of Shielding" : Equip("Brooch of Shielding", 0, 5, [acs["Arcane Shield"]], upgradedForm = upgradedAccs["Barrier Necklace"]),
+    "Brooch of Shielding" : Equip("Brooch of Shielding", 0, 5, [acs["Arcane Shield"]], upgradedForm = tier2Accs["Barrier Necklace"]),
 
     # Utility
-    "Hastening Amulet" : Equip("Hastening Amulet", 0, 0, actionBoost=1, upgradedForm = upgradedAccs["Lightspeed Amulet"]),
-    "Shoving Gauntlets" : Equip("Shoving Gauntlets", 0, 0, [acs["Shove"]], speedBoost=1, upgradedForm = upgradedAccs["Repelling Gloves"])
+    "Hastening Amulet" : Equip("Hastening Amulet", 0, 0, actionBoost=1, upgradedForm = tier2Accs["Lightspeed Amulet"]),
+    "Shoving Gauntlets" : Equip("Shoving Gauntlets", 0, 0, [acs["Shove"]], speedBoost=1, upgradedForm = tier2Accs["Repelling Gloves"])
 
     # Offensive
 
 }
 
 # Set slots
-for item in utils.merge(weps.values(), upgradedWeps.values(), enemWeps.values()):
+for item in utils.merge(weps.values(), tier2Weps.values(), enemWeps.values()):
     item.slot = "Mainhand"
-for item in utils.merge(armors.values(), upgradedArmors.values(), enemArmrs.values()):
+for item in utils.merge(armors.values(), tier2Armors.values(), enemArmrs.values()):
     item.slot = "Armor"
-for item in utils.merge(offs.values(), upgradedOffs.values()):
+for item in utils.merge(offs.values(), tier2Offs.values()):
     item.slot = "Offhand"
-for item in utils.merge(accs.values(), upgradedAccs.values(), enemAcs.values()):
+for item in utils.merge(accs.values(), tier2Accs.values(), enemAcs.values()):
     item.slot = "Accessory"
 
 # Loot Pools
 lootPools = {
     "Standard" : utils.merge(weps.values(), armors.values(), offs.values(), accs.values()),
-    "Upgraded" : utils.merge(upgradedWeps.values(), upgradedArmors.values(), upgradedOffs.values(), upgradedAccs.values())
+    "Upgraded" : utils.merge(tier2Weps.values(), tier2Armors.values(), tier2Offs.values(), tier2Accs.values())
 }
 
 
